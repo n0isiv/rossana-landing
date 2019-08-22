ce702f85c58815d6ce4c1fd8f786b7fa36a9525a
Niels Grieger <niels.grieger@rockstardevelopers.de>
2019-08-22 13:36:41 +0200

    feat: create typewrite animation

---
+++ assets/js/typewriter.js
@@ -0,0 +1,61 @@
+var TypewriterAnimation = function(el, rotatingText) {
+  this.el = el;
+  this.rotatingText = rotatingText;
+  this.currentLoop = 0;
+  this.isReverting = false;
+  this.period = 100;
+  this.text = "";
+  this.animate();
+};
+
+TypewriterAnimation.prototype.animate = function() {
+  var visibleTextIndex = this.currentLoop % this.rotatingText.length;
+  var fullText = this.rotatingText[visibleTextIndex];
+
+  this.period = 100;
+  if (this.isReverting) {
+    this.text = fullText.substring(0, this.text.length - 1);
+  } else {
+    this.text = fullText.substring(0, this.text.length + 1);
+  }
+
+  this.el.text(this.text);
+  this.el.css("transition", "")
+  this.el.css("max-width", this.el[0].scrollWidth)
+
+  if (!this.isReverting && this.text === fullText) {
+    this.isReverting = true;
+    this.period = 1500;
+    this.text = ""
+    this.el.css("animation", "animated-cursor 600ms ease-in-out infinite")
+    // this.el.css("max-width", "0")
+  } else if (this.isReverting && this.text === "") {
+    this.isReverting = false;
+    this.currentLoop++;
+    this.el.css("animation", "none")
+    this.el.css("transition", "none")
+    this.el.css("max-width", "0")
+  }
+
+  setTimeout(
+    function() {
+      this.animate();
+    }.bind(this),
+    this.period
+  );
+};
+
+(function($) {
+  var $window = $(window);
+  var $typewriterElms = $(".typewriter-animation");
+
+  // initialize the typewriter animation
+  $window.on("load", function() {
+    for (var i = 0; i < $typewriterElms.length; i++) {
+      var $typewriterElm = $($typewriterElms[i]);
+      var rotatingText = $typewriterElm.data("rotate");
+
+      new TypewriterAnimation($typewriterElm, rotatingText);
+    }
+  });
+})(jQuery);
