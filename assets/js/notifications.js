851bc1b6072f0c9536d67cacc4d814a2095ce030
Niels Grieger <niels.grieger@pm.me>
2019-09-19 08:05:19 +0200

    feat: hide notification after 5 sec

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,6 +1,7 @@
 function queryParam(name) {
-  var results = new RegExp('[\?&]' + name + '=([^&#]*)')
-                    .exec(window.location.href);
+  var results = new RegExp("[?&]" + name + "=([^&#]*)").exec(
+    window.location.href
+  );
 
   if (results !== null) {
     return results[1];
@@ -10,13 +11,29 @@ function queryParam(name) {
 (function($) {
   var $window = $(window);
 
+  function hideNotification($notification) {
+    $notification.removeClass("notification--visible");
+  }
+
   $window.on("load", function() {
     var notificationType = queryParam("notification");
 
     if (notificationType) {
-      var $notification = $(`.notifications [data-type="${notificationType}"]`)
+      var $notification = $(`.notifications [data-type="${notificationType}"]`);
+      var $notificationClose = $notification.find(".notification__close");
+      var hrefWithoutQuery = window.location.href.split("?")[0];
 
       $notification.addClass("notification--visible");
+      $notificationClose.on("click", function(ev) {
+        ev.preventDefault();
+        hideNotification($notification);
+
+        window.location = hrefWithoutQuery;
+      });
+
+      setTimeout(function() {
+        hideNotification($notification);
+      }, 5000);
     }
   });
-})(jQuery)
+})(jQuery);
